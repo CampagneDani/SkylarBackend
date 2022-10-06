9673824fd36b609b3037435f1f28d4d7dda063a2
usirprotzm <robin.protzmann@usu.com>
2022-10-06 12:15:08 +0200

    Change: cors config

--- src/main/java/org/example/BackEndSkylar/BackEndSkylarApplication.java
+++ src/main/java/org/example/BackEndSkylar/BackEndSkylarApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -23,6 +24,7 @@ public class BackEndSkylarApplication {
 	public CorsFilter corsFilter(){
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 		corsConfiguration.setAllowedOrigins(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
