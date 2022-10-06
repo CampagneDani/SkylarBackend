2d1cf35d63a7dbca3fdfc6c89936f89a03d96de6
Y0UD3ADBR0 <lukas-hellstern@live.de>
2022-10-06 11:31:33 +0200

    Change: url auf /user

--- src/main/java/org/example/BackEndSkylar/BenutzerResource.java
+++ src/main/java/org/example/BackEndSkylar/BenutzerResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/benutzer")
+@RequestMapping("/user")
 public class BenutzerResource {
     private final BenutzerService benutzerService;
 
